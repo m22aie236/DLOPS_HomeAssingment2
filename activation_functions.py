23e16fdc1f6e78fab0687a55eac6c4d83dcce660
M22AIE236 <m22aie236@iitj.ac.in>
2024-03-30 16:51:43 +0530

    activity 1 done M22AIE236

--- activation_functions.py
+++ activation_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import feature1
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -14,20 +15,12 @@ def tanh(x):
     return np.tanh(x)
 
 
+
 # Modify activation_functions.py
-# Import necessary libraries (already imported in the script)
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Define input range
-random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
-
-# Apply activation functions to the input data
 relu_output = relu(np.array(random_values))
 leaky_relu_output = leaky_relu(np.array(random_values))
 tanh_output = tanh(np.array(random_values))
 
-# Plot the output of each activation function
 plt.plot(random_values, relu_output, label='ReLU')
 plt.plot(random_values, leaky_relu_output, label='Leaky ReLU')
 plt.plot(random_values, tanh_output, label='Tanh')
--- feature1.py
+++ feature1.py
@@ -1,6 +1,7 @@
 # feature1.py
 import numpy as np
 import matplotlib.pyplot as plt
+import activation_functions
 
 random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
